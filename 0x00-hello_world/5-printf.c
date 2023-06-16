58ba2b24bcb56c15c1a220f7873d58e239acb0eb
Emmanuel <mrpresidentemmanuel@gmail.com>
2023-06-16 15:03:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Description: Print "with proper grammar, but the outcome is a piece of art,"
+ * , with \n.
+ * Return: 0 (this indicate success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
