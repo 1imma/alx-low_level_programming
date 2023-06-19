9b705f1996ec75046bf4e26f037f7672458d433a
Emmanuel <mrpresidentemmanuel@gmail.com>
2023-06-19 02:22:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Descriptio: print lowercase and uppercase alphabent
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	/* Print lowercase alphabet */
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	/* Print uppercase alphabet */
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
