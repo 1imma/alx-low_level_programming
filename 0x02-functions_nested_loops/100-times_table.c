7d8fe6664f9f0fa8ab0bc08c6345c4148c8131c0
Emmanuel <mrpresidentemmanuel@gmail.com>
2023-06-21 16:16:56 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_times_table - Prints the n times table, starting with 0
@@ -6,28 +6,41 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int result;
+    int i, j, result;
 
-	if (n < 0 || n > 15)
-		return;
+    if (n < 0 || n > 15)
+        return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
-					_putchar(' ');
-			}
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
+            if (j == 0)
+                _putchar('0');
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (result >= 100)
+                {
+                    _putchar(result / 100 + '0');
+                    _putchar((result / 10) % 10 + '0');
+                }
+                else if (result >= 10)
+                {
+                    _putchar(' ');
+                    _putchar(result / 10 + '0');
+                }
+                else
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                _putchar(result % 10 + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
+
