482671b32d5ef363dfcdc22d472fd09471e76ee9
Emmanuel <mrpresidentemmanuel@gmail.com>
2023-06-26 13:05:02 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed.
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
